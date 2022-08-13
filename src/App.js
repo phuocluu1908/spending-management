1059b6da407d11a965a511d97bfa909c695fdac8
Phuoc Luu <luu@blueberrylabs.com>
2022-08-13 13:52:06 +0700

    remove login page from app and remove log

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-import LoginPage from "./components/page/LoginPage";
 import SpendingAdd from "./components/page/SpendingAdd";
 import SpendingList from "./components/page/SpendingList";
 
 function App() {
   const [isAdd, setIsAdd] = useState(false);
-
   const backToListPage = () => setIsAdd(false)
-  console.log('just for test purpose')
+
   if (isAdd) {
     return <SpendingAdd backToListPage={backToListPage} />;
   }
