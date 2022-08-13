a7ff0a85f2efd74d5e837b4f851dd7d98b9ea6e9
Phuoc Luu <luu@blueberrylabs.com>
2022-08-13 15:09:00 +0700

    remove counter from store

--- src/store/Counter/index.js
+++
@@ -1,6 +0,0 @@
-import { counterActions, counterSlice as CounterSlice } from "./CounterManager"
-import counterReducer from './CounterManager'
-
-export const { increment, decrement, incrementByAmount } = counterActions
-export const counterSlice = CounterSlice
-export default counterReducer
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './Counter'
 import spendingReducer from './Spending'
 import screenReducer from './Screen'
 
 export const store = configureStore({
   reducer: {
-      counter: counterReducer,
       spending: spendingReducer,
       screen: screenReducer
   },
